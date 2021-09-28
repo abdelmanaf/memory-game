a39b958c8e4c28b59c79006917e4cd7f1c4712d6
abdelmanaf <ourotoumanaf6@gmail.com>
2021-09-27 22:59:27 -0400

    add all the app's states variable and cached elements

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/ 
+
+
+
+
+/*----- app's state (variables) -----*/ 
+
+ let click = 0;  // state to kn
+ let colors = []; // array of colors
+ let refs = [] // array of dataset reference
+ let matched = 0; // need to know when the game is over
+
+
+
+/*----- cached element references -----*/ 
+const gameEl = document.querySelector("#game");  // get all elements
+const buttonEl = document.querySelector("#reset-btn"); // get reset button element
+const messageEl = document.querySelector(".message");
+

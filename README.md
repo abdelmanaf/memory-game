c9cd2f78fb9671a2fff420a2db90ade5cd65141e
abdelmanaf <ourotoumanaf6@gmail.com>
2021-09-28 15:52:17 -0400

    add a readMe file

---
+++ README.md
@@ -0,0 +1,17 @@
+# Memory-Game
+
+This game is base on matching colors. In this game, you will have the option to chose the card from the board.
+If the card color match your second choice, them the two cards will stay open until the game finish. And so on.
+In the case the cards don't match,they flip over; then you get another chance to guess others cards.
+
+#####Chose carefully!
+
+### Screenshoots
+![memory-game start image](https://i.imgur.com/0xzK9sI.png)
+![memory-game middle image](https://i.imgur.com/B6sfsaZ.png)
+![memory-game end image](https://i.imgur.com/JPPRaxQ.png)
+
+### Technologies used
+JavaScript, 
+HTML, 
+CSS
--- js/main.js
+++ js/main.js
@@ -29,10 +29,9 @@ function resetState(){
   refs = [];
 }
 
+
 function handleClick (e){
-  console.log(e)
     const el = e.target;
-    // console.log(el.dataset.hidden);
     const isHidden = el.dataset.hidden;
     const getColor = el.dataset.color;
     const getRef = el.dataset.ref;
@@ -40,36 +39,36 @@ function handleClick (e){
     refs.push(getRef)
 
     if(isHidden === "true"){
-        if (click == 0){ //check if it's the first time
+        if (click === 0){ //check if it's the first time
            el.classList.remove('color-hidden');
            el.dataset.hidden = "false";
           click++
         } else {
-          if(click == 1){
-           if(colors[0] == colors[1]){
+          if(click === 1){
+           if(colors[0] === colors[1]){
 
              el.classList.remove('color-hidden');
              el.dataset.hidden = "false";
              matched++
            
              resetState();
-              if(matched == 8){
-                messageEl.textContent = "Congratulations , you win";
+              if(matched === 8){
+                messageEl.textContent = "Congratulations, you win!!!";
              }
-            }else {
+          }else {
               const ref = document.querySelector(`[data-ref='${refs[0]}`);
-               ref.classList.add('color-hidden')
-               ref.dataset.hidden = "true";
-               resetState();
-           }
+              ref.classList.add('color-hidden')
+              ref.dataset.hidden = "true";
+              resetState();
+          }
           }
         }
-     
-    } else {
-        
-        alert('it already selected');
+    } else { 
+              alert('It is already selected');
     }
 }
+
+
 
 function resetAllElements(){
   

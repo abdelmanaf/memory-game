d68f73724ab68cdc9d049f1f378118b143fc3497
abdelmanaf <ourotoumanaf6@gmail.com>
2021-09-28 08:54:19 -0400

    add handleClick function.it holds all the logic of my game

--- js/main.js
+++ js/main.js
@@ -29,6 +29,48 @@ function resetState(){
   refs = [];
 }
 
+function handleClick (e){
+  console.log(e)
+    const el = e.target;
+    // console.log(el.dataset.hidden);
+    const isHidden = el.dataset.hidden;
+    const getColor = el.dataset.color;
+    const getRef = el.dataset.ref;
+    colors.push(getColor)
+    refs.push(getRef)
+
+    if(isHidden === "true"){
+        if (click == 0){ //check if it's the first time
+           el.classList.remove('color-hidden');
+           el.dataset.hidden = "false";
+          click++
+        } else {
+          if(click == 1){
+           if(colors[0] == colors[1]){
+
+             el.classList.remove('color-hidden');
+             el.dataset.hidden = "false";
+             matched++
+           
+             resetState();
+              if(matched == 8){
+                messageEl.textContent = "Congratulations , you win";
+             }
+            }else {
+              const ref = document.querySelector(`[data-ref='${refs[0]}`);
+               ref.classList.add('color-hidden')
+               ref.dataset.hidden = "true";
+               resetState();
+           }
+          }
+        }
+     
+    } else {
+        
+        alert('it already selected');
+    }
+}
+
 function resetAllElements(){
   
   const els = document.querySelectorAll('[data-ref]');

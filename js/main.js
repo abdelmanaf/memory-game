56c59bcc15fd8fa9d55bb5eb3fe0d302de4c731f
abdelmanaf <ourotoumanaf6@gmail.com>
2021-09-27 23:19:20 -0400

    add events listenners and a function called resetAllElements

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 
 /*----- app's state (variables) -----*/ 
 
- let click = 0;  // state to kn
+ let click = 0;  
  let colors = []; // array of colors
  let refs = [] // array of dataset reference
  let matched = 0; // need to know when the game is over
@@ -17,8 +17,25 @@ const gameEl = document.querySelector("#game");  // get all elements
 const buttonEl = document.querySelector("#reset-btn"); // get reset button element
 const messageEl = document.querySelector(".message");
 
+/*----- event listeners -----*/ 
+gameEl.addEventListener("click", handleClick); // listen any click event of game elements
+buttonEl.addEventListener("click", resetAllElements);
+
+
+/*----- functions -----*/
 function resetState(){
   click = 0 ;
   colors = [];
   refs = [];
+}
+
+function resetAllElements(){
+  
+  const els = document.querySelectorAll('[data-ref]');
+   els.forEach(el =>{
+       el.classList.add('color-hidden');
+       el.dataset.hidden = true;
+  })
+  messageEl.textContent = "";
+
 }
